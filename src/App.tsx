a05350382f74c33dbda9cabfe9c1dca106fdd794
Anne-Hyeyeon <khy2106@naver.com>
2023-11-29 18:10:36 +0900

    Chores: 불필요한 img assets 제거

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,6 @@ import { CssBaseline } from '@mui/material';
 import Router from './router/router';
 
 const App = () => {
-  // function setScreenSize() {
-  //   const vh = window.innerHeight * 0.01;
-  //   document.documentElement.style.setProperty('--vh', `${vh}px`);
-  // }
-
-  // useEffect(() => {
-  //   setScreenSize();
-  //   window.addEventListener('resize', setScreenSize);
-  //   return () => {
-  //     window.removeEventListener('resize', setScreenSize);
-  //   };
-  // }, []);
-
   return (
     <BrowserRouter>
       <CssBaseline />
