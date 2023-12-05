2359cd680ad248c11b7e61af0b39cbaac2b11557
Anne-Hyeyeon <khy2106@naver.com>
2023-12-05 20:23:32 +0900

    Feat: preview 기능 추가

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,24 +1,26 @@
-import { getFontEmbedCSS, toJpeg, toPng } from 'html-to-image';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { storage } from '../firebase';
 import { deleteObject } from 'firebase/storage';
 import EmphasizedPoster from '../common/components/Poster/EmphasizedPoster';
 import { State, init } from '../store/store';
 import SimplePoster from '../common/components/Poster/SimplePoster';
+import MainWrapper from '../common/components/MainWrapper';
+import { getPosterWidth } from '../common/function/getPosterWidth';
+import { Box, Button } from '@mui/material';
+import { toJpeg } from 'html-to-image';
 
 const Result = () => {
   const dispatch = useDispatch();
   const state = useSelector((state: State) => state);
-  const ref = useRef<HTMLDivElement>(null);
+  const ref = useRef<HTMLBRElement>(null);
+
   const { fileRef, posterType } = state;
 
   const onButtonClick = useCallback(() => {
     if (ref.current === null) {
       return;
     }
-    const actualWidth = ref.current.offsetWidth; // 실제 컨텐츠의 너비
-    const actualHeight = ref.current.offsetHeight; // 실제 컨텐츠의 높이
+    const actualHeight = ref.current.offsetHeight;
 
     toJpeg(ref.current, {
       width: 795,
@@ -44,10 +46,13 @@ const Result = () => {
   const storedValue = sessionStorage.getItem('showFullPage');
   const initialShowFullPage = storedValue === 'true';
   const [showFullPage, setShowFullPage] = useState(initialShowFullPage);
+  const [posterWidth, setPosterWidth] = useState(getPosterWidth(60, 100));
 
-  const displayPoster = () => {
-    if (posterType === 'emphasized') return <EmphasizedPoster />;
-    if (posterType === 'simple') return <SimplePoster />;
+  const displayPreviewPoster = () => {
+    if (posterType === 'emphasized')
+      return <EmphasizedPoster preview styles={{ width: posterWidth }} />;
+    if (posterType === 'simple')
+      return <SimplePoster styles={{ width: posterWidth }} />;
   };
 
   useEffect(() => {
@@ -78,47 +83,52 @@ const Result = () => {
     };
   }, [fileRef]);
 
-  if (!showFullPage) {
-    return (
-      <div style={{ height: '300px', width: '150px', margin: 'auto' }}>
-        <div
-          style={{
-            height: '300px',
-            width: '300px',
-            overflow: 'hidden',
-            position: 'relative',
-          }}
-        >
-          <div style={{ marginTop: 50 }}>{displayPoster()}</div>
-          <div
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-              background: 'rgba(0, 0, 0, 0.5)',
-            }}
-          ></div>
-        </div>
-        <button onClick={onShowFullPageClick}>
-          쿠팡 보고 포스터 결과물 보기
-        </button>
-      </div>
-    );
-  }
+  useEffect(() => {
+    const handleResize = () => {
+      setPosterWidth(getPosterWidth(100, 100));
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <>
-      <div ref={ref}>{displayPoster()}</div>
-      <button onClick={onButtonClick}>Click me</button>
-      <button
-        onClick={() => {
-          dispatch(init());
-        }}
-      >
-        다시 하기
-      </button>
+      {!showFullPage ? (
+        <>
+          <Box ref={ref}>{displayPreviewPoster()}</Box>
+          <>
+            <button onClick={onButtonClick}>Click me</button>
+            <button
+              onClick={() => {
+                dispatch(init());
+              }}
+            >
+              다시 하기
+            </button>
+          </>
+        </>
+      ) : (
+        <Box height="100vh">
+          <MainWrapper>
+            <Box>
+              <Box
+                style={{
+                  position: 'relative',
+                }}
+              >
+                <Box>{displayPreviewPoster()}</Box>
+              </Box>
+              <Button onClick={onShowFullPageClick}>
+                쿠팡 보고 포스터 결과물 보기
+              </Button>
+            </Box>
+          </MainWrapper>
+        </Box>
+      )}
     </>
   );
 };
