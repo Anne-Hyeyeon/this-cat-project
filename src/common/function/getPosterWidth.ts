66bf8723ff8148cafbeb913e1a1e73f380bd6f80
Anne-Hyeyeon <khy2106@naver.com>
2023-12-28 16:20:57 +0900

    Chore: 코드 주석 업데이트

--- src/common/function/getPosterWidth.ts
+++ src/common/function/getPosterWidth.ts
@@ -1,4 +1,5 @@
 export const getPosterWidth = (xsWidth: number, smWidth: number): number => {
+  // 스크린 화면의 크기를 체크한 후, 화면에 맞는 포스터 크기를 리턴
   const screenWidth = window.innerWidth;
 
   if (screenWidth >= 600) {
